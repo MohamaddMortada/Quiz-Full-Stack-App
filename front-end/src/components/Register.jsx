7ad3025b0c1bd2f6a01c270e76eecff8f82b2326
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-09 01:13:20 +0200

    adding Register.jsx

---
+++ front-end/src/components/Register.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../App.css';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`http://localhost:8000/api/register`, { username, password });
+      //take user to quiz page
+    } catch (error) {
+      console.error('Error registering:', error);
+      alert('Registration failed');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} required />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
